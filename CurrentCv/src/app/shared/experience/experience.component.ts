4d818c2f73fe9ce2436108933b9f40c0165bda83
Vigen <vigenhovhannisyan@gmail.com>
2023-03-28 11:10:22 +0400

    added experience

--- CurrentCv/src/app/shared/experience/experience.component.ts
+++ CurrentCv/src/app/shared/experience/experience.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'cv-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss', './experience.component.media.scss']
 })
 export class ExperienceComponent implements OnInit {
 
